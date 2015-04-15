ba8e658c0189b0f16688416f0ff98ca03c1314f4
Subinkrishna Gopi <subinkrishna.gopi@gmail.com>
2015-04-15 11:30:43 -0600

    Optimizations to mimic fragment transitions

--- app/src/main/java/com/toolbartest/MainActivity.java
+++ app/src/main/java/com/toolbartest/MainActivity.java
@@ -7,23 +7,28 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity implements DrawerFragment.OnNavigationChangeListener {
 
-    Toolbar toolbar;
+    private Toolbar mToolbar;
+    private TextView mTextView;
+    private DrawerLayout mDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app_bar);
-        toolbar = (Toolbar)findViewById(R.id.app_bar);
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        mTextView = (TextView) findViewById(R.id.text);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        setSupportActionBar(toolbar);
+        setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Home");
         DrawerFragment drawerFragment = (DrawerFragment)getSupportFragmentManager().findFragmentById(R.id.drawer_fragment);
         
-        drawerFragment.setUp((DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
+        drawerFragment.setUp((DrawerLayout)findViewById(R.id.drawer_layout), mToolbar);
     }
 
 
@@ -52,5 +57,7 @@ public class MainActivity extends ActionBarActivity implements DrawerFragment.On
     @Override
     public void onNavigationChange(String label) {
         Log.d("TEST", "Clicked nav item: " + label + ". Change fragment!");
+        mDrawer.closeDrawers();
+        mTextView.setText(label);
     }
 }
